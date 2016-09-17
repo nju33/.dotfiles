13ed0dc1d5900bec2d61dd83e17236b0b89be856
nju33 <nju33.ki@gmail.com>
2016-09-18 08:39:25 +0900

    Fix lns.sh

--- lns.sh
+++ lns.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-function del() {
-  rm -rf ~/$1 >/dev/null 2>&1
-}
-
 function run() {
   local files=(
     .zshrc .zsh.d .vimrc .hyperterm.js hyperterm
     emmet .npmrc
   )
-  for t in ${files[@]}; do del $t; done
-  for t in ${files[@]}; do ln -s `pwd`/$t ~/$t; done
+  for f in ${files[@]}; do
+    rm -rf ~/$f >/dev/null 2>&1
+    ln -s `pwd`/$f ~/$f
+  done
 }
-
 run
