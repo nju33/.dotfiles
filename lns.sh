5a0df0ec510a18da88aedb06ac63c085e2b1dc89
nju33 <nju33.ki@gmail.com>
2016-09-18 07:04:18 +0900

    Add emmet/

--- lns.sh
+++ lns.sh
@@ -4,10 +4,12 @@ rm ~/.zshrc
 rm -rf ~/.zsh.d
 rm ~/.vimrc
 rm ~/.hyperterm.js
-rm ~/hyperterm
+rm -rf ~/hyperterm
+rm -rf ~/emmet
 
 ln -s `pwd`/.zshrc ~/.zshrc
 ln -s `pwd`/.zsh.d ~/.zsh.d
 ln -s `pwd`/.vimrc ~/.vimrc
 ln -s `pwd`/.hyperterm.js ~/.hyperterm.js
 ln -s `pwd`/hyperterm ~/hyperterm
+ln -s `pwd`/emmet ~/emmet
