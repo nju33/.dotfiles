6bb2e6b1d07f6b2bd2e5c764d9a79692a5150c74
nju33 <nju33.ki@gmail.com>
2017-03-14 22:29:27 +0900

    Refact .hyperterm.js

--- .hyperterm.js
+++ .hyperterm.js
@@ -20,61 +20,10 @@ module.exports = {
     borderColor: '#bd4a4d',
 
     // custom css to embed in the main window
-    css: `
-    //   .hyperterm_main:before {
-    //     content: "";
-    //     position: fixed;
-    //     right: 20px;
-    //     bottom: 0;
-    //     height: 70vh;
-    //     width: 30vw;
-    //     background-image: url(/Users/nju33/hyperterm/images/amami-haruka/kimono.png);
-    //     background-position: right bottom;
-    //     background-size: contain;
-    //     background-repeat: no-repeat;
-    //     border-bottom: 1px solid #bd4a4d;
-    //     transform: rotateY(180deg);
-    //   }
-     //
-    //   .tabs_nav {
-    //     font-weight: bold;
-    //     font-size: 10px;
-    //     font-family: 'SourceHanCodeJP-Normal';
-    //   }
-     //
-    //  .tabs_title {
-    //    color: #bd4a4d;
-    //    font-size: 1.2em;
-    //  }
-     //
-    //  .tab_tab {
-    //    color: #888;
-    //  }
-     //
-    //  .tab_active {
-    //    font-size: 1.2em;
-    //    color: #bd4a4d;
-    //  }
-     //
-    //  .tab_active:hover {
-    //    color: #bd4a4d;
-    //  }
-     //
-    //  .tab_active .tab_icon {
-    //    top: 11px;
-    //    /*border: 1px solid;*/
-    //    color: #bd4a4d;
-    //    cursor: pointer;
-    //  }
-    `,
+    css: ``,
 
     // custom css to embed in the terminal window
-    termCSS: `
-      // x-row {
-      //   font-weight: bolder;
-      //   background: #111;
-      // }
-    `,
+    termCSS: ``,
 
     // custom padding (css format, i.e.: `top right bottom left`)
     padding: '1em',
@@ -107,6 +56,8 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
+  bell: false,
+
   plugins: [
     'hyperterm-atom-dark',
     'hyperterm-alternatescroll'
