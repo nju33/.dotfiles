91ddb9f02efb1c08ca5fb4a91070263273f2b655
nju33 <nju33.ki@gmail.com>
2019-02-20 11:08:38 +0900

    fix

--- .config_fish_config.fish
+++ .config_fish_config.fish
@@ -9,7 +9,7 @@ set -gx PGUSER postgres
 set -gx PGPORT 54321
 set -gx PGPASSWORD ''
 function pg_server --description 'docker run pg'
-  docker run --rm -p 54321:5432 postgres
+  docker run --rm -p 54321:5432 nju33/postgres
 end
 
 function chrome-dev --description 'open chrome-canary in devmode'
